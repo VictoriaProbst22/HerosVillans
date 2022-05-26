df05f26235682d8492aacbcced4b29a154122ce4
Victoria Probst <victoria.probst13@gmail.com>
2022-05-26 15:27:53 -0400

    updated project URLS and created first View Function and created Super Types URLs file

--- super_types/views.py
+++ super_types/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def super_type_list(request):
+
+
+    return Response('ok')
