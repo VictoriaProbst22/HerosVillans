78ac1a871830aee4aad762219fd4b2319f6fbe54
Victoria Probst <victoria.probst13@gmail.com>
2022-05-26 14:37:50 -0400

    Created Super and Super Types Models and made migrations for tables to be created

---
+++ supers/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from super_types.models import Super_types
+
+# Create your models here.
+class Super(models.Model):
+    name = models.CharField(max_length=200)
+    alter_ego = models.CharField(max_length=200)
+    primary_ability = models.CharField(max_length=200)
+    secondary_ability = models.CharField(max_length=200)
+    catch_phrase = models.CharField(max_length=200)
+    super_type = models.ForeignKey(Super_types, on_delete= models.CASCADE, null=True)
