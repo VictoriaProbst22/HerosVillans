6baf9665e84dc2c7f2a5b833e871762e1c9a88bb
Victoria Probst <victoria.probst13@gmail.com>
2022-05-26 17:58:43 -0400

    Created SuperTypes model and migrations

--- supers/models.py
+++ supers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from super_types.models import Super_types
 #from super_types.models import Super_types
 
 # Create your models here.
@@ -8,4 +9,4 @@ class Super(models.Model):
     primary_ability = models.CharField(max_length=200)
     secondary_ability = models.CharField(max_length=200)
     catch_phrase = models.CharField(max_length=200)
-   
+    super_type = models.ForeignKey(Super_types, on_delete=models.CASCADE, null=True)
